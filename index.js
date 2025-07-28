5e3d6e3fe38cd9cd4aaddc6d9f4798e4149b08e7
adiya8 <adiyaa769@gmail.com>
2025-07-28 12:23:57 +0800

    ingsn

---
+++ index.js
@@ -0,0 +1,10 @@
+var twoSum = function (nums, target) {
+  let map = {};
+  for (let i = 0; i < nums.length; i++) {
+    let complement = target - nums[i];
+    if (map.hasOwnProperty(complement)) {
+      return [map[complement], i];
+    }
+    map[nums[i]] = i;
+  }
+};
